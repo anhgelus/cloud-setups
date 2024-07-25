cd87d72cc17b0079f05045ca52c3a95b2d480772
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-07-25 15:01:31 +0000

    feat(minecraft): install java

---
+++ minecraft/only-java/java21-rocky.sh
@@ -0,0 +1,14 @@
+#/usr/bin/bash
+
+sudo -i
+cat <<EOF > /etc/yum.repos.d/adoptium.repo
+[Adoptium]
+name=Adoptium
+baseurl=https://packages.adoptium.net/artifactory/rpm/${DISTRIBUTION_NAME:-$(. /etc/os-release; echo $ID)}/\$releasever/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+EOF
+
+dnf install temurin-21-jre
+
