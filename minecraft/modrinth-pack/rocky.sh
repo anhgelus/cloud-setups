87927c7ec9960c1aab317ebfbabaa86790ea4154
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-07-25 16:20:28 +0000

    fix(minecraft): 404 error on adoptium repo and wrong go version

--- minecraft/modrinth-pack/rocky.sh
+++ minecraft/modrinth-pack/rocky.sh
@@ -1,18 +1,16 @@
 #/usr/bin/bash
 
-sudo -i
 cat <<EOF > /etc/yum.repos.d/adoptium.repo
 [Adoptium]
 name=Adoptium
-baseurl=https://packages.adoptium.net/artifactory/rpm/${DISTRIBUTION_NAME:-$(. /etc/os-release; echo $ID)}/\$releasever/\$basearch
+baseurl=https://packages.adoptium.net/artifactory/rpm/rhel/\$releasever/\$basearch
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
 EOF
 
-dnf -y install temurin-$JAVA_VERSION-jre go
-go install github.com/nothub/mrpack-install@latest
-$p = $(go env GOPATH)/bin
-chmod +x $p/mrpack-install
-$p/mrpack-install $PACK_LINK
+dnf -y install temurin-$JAVA_VERSION-jre
+curl -o mrpack-install https://github.com/nothub/mrpack-install/releases/download/v0.16.10/mrpack-install-linux
+chmod +x mrpack-install 
+./mrpack-install $PACK_LINK
 
--- minecraft/only-java/rocky.sh
+++ minecraft/only-java/rocky.sh
@@ -1,10 +1,9 @@
 #/usr/bin/bash
 
-sudo -i
 cat <<EOF > /etc/yum.repos.d/adoptium.repo
 [Adoptium]
 name=Adoptium
-baseurl=https://packages.adoptium.net/artifactory/rpm/${DISTRIBUTION_NAME:-$(. /etc/os-release; echo $ID)}/\$releasever/\$basearch
+baseurl=https://packages.adoptium.net/artifactory/rpm/rhel/\$releasever/\$basearch
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
