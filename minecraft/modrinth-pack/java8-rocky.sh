b97d73078ae8f8381e0071daea134c6ebbf5577e
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-07-25 14:58:16 +0000

    feat(minecraft): install modrinth pack for rockylinux

---
+++ minecraft/modrinth-pack/java8-rocky.sh
@@ -0,0 +1,18 @@
+#/usr/bin/bash
+
+sudo -i
+cat <<EOF > /etc/yum.repos.d/adoptium.repo
+[Adoptium]
+name=Adoptium
+baseurl=https://packages.adoptium.net/artifactory/rpm/${DISTRIBUTION_NAME:-$(. /etc/os-release; echo $ID)}/\$releasever/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+EOF
+
+dnf install temurin-8-jre go
+go install github.com/nothub/mrpack-install@latest
+$p = $(go env GOPATH)/bin
+chmod +x $p/mrpack-install
+$p/mrpack-install $PACK_LINK
+
