9e70a333d6607f9e10542f464de8bbe7b18f8a6c
William Hergès <anhgelus@anhgelus.world>
2025-06-29 14:43:46 +0200

    fix(minecraft): not installing mrpack-install

--- minecraft/modrinth-pack/java21-rocky.sh
+++ minecraft/modrinth-pack/java21-rocky.sh
@@ -11,6 +11,7 @@ EOF
 
 sudo dnf -y install temurin-21-jre wget
 wget -O mrpack-install.rpm https://github.com/nothub/mrpack-install/releases/download/v0.21.0-beta/mrpack-install_0.21.0-beta_linux_amd64.rpm 
-dnf install mrpack-install.rpm
-/usr/local/bin/mrpack-install $PACK_LINK
+dnf install -y mrpack-install.rpm
+rm mrpack-install.rpm
+/usr/local/bin/mrpack-install $PACK_LINK --server-dir /var/minecraft
 
--- minecraft/modrinth-pack/java8-rocky.sh
+++ minecraft/modrinth-pack/java8-rocky.sh
@@ -11,5 +11,6 @@ EOF
 
 sudo dnf -y install temurin-8-jre wget
 wget -O mrpack-install.rpm https://github.com/nothub/mrpack-install/releases/download/v0.21.0-beta/mrpack-install_0.21.0-beta_linux_amd64.rpm 
-dnf install mrpack-install.rpm
-/usr/local/bin/mrpack-install $PACK_LINK
+dnf install -y mrpack-install.rpm
+rm mrpack-install.rpm
+/usr/local/bin/mrpack-install $PACK_LINK --server-dir /var/minecraft
